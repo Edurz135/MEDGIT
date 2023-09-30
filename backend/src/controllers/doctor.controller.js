a47845e8692dfd9dce07190c5c2c3ae9a25ef48d
Nicolas Xavier Herrera Medina <nicoxavier2013@hotmail.com>
2023-09-30 01:53:10 -0500

    Agregue para las citas pasadas

---
+++ backend/src/controllers/doctor.controller.js
@@ -0,0 +1,12 @@
+const getPastAppointments = async (req, res) => {
+    try {
+        const result = await getPastAppointmentsService(req.body);
+        return res.status(200).json({
+          status: 200,
+          result: result,
+          message: "Succesfully Appointsments Returned",
+        });
+      } catch (e) {
+        return res.status(400).json({ status: 400, message: e.message });
+      } 
+}
