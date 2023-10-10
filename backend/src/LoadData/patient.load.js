4b841459104d6f3d5068f82a7768d798d2555821
Eduardo Ramón <edu.rz@hotmail.com>
2023-10-10 10:08:13 -0500

    error handling on data load

--- backend/src/LoadData/patient.load.js
+++ backend/src/LoadData/patient.load.js
@@ -1,40 +1,53 @@
 const Patient = require("../models/patient.model");
 const { faker } = require('@faker-js/faker');
-const routes =require("express").routes();
+const routes = require("express").routes();
 
-routes.get("/patient",async(req,res) =>{
-    const patients = await Patient.findAll();
-    res.status(200).json({
-        ok:true,
-        status:200,
-        body:patients,
-    })
+routes.get("/patient", async (req, res) => {
+    try {
+        const result = await Patient.findAll();
+        res.status(200).json({
+            status: 200,
+            result: result,
+            message: "Succesfully Patients Returned"
+        })
+    } catch (e) {
+        return res.status(400).json({ status: 400, message: e.message });
+    }
 });
-routes.get("/patient/:id",async(req,res) =>{
-    const id=req.params.id;
-    const patient=await Patient.findOne({
-        where:{
-            id,
-        }
-    });
-    res.status(200).json({
-        ok:true,
-        status:200,
-        body:patient,
-    })
+routes.get("/patient/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const result = await Patient.findOne({
+            where: {
+                id,
+            }
+        });
+        res.status(200).json({
+            status: 200,
+            result: result,
+            message: "Succesfully Patient Returned"
+        })
+    } catch (e) {
+        return res.status(400).json({ status: 400, message: e.message });
+    }
 });
-routes.post("/patient",async(req,res)=>{
-    await Patient.sync()
-    const createPatient =await Patient.create({
-        name: faker.internet.userName(),
-        email:faker.internet.email(),
-        password:faker.internet.password(),
-    })
-    res.status(201).json({
-        ok:true,
-        status:200,
-        message:"Created Product",
-    })
+routes.post("/patient", async (req, res) => {
+
+    try {
+        await Patient.sync()
+        const result = await Patient.create({
+            name: faker.internet.userName(),
+            email: faker.internet.email(),
+            password: faker.internet.password(),
+        })
+        res.status(201).json({
+            status: 200,
+            result: result,
+            message: "Succesfully Patient Created"
+        })
+    } catch (e) {
+        return res.status(400).json({ status: 400, message: e.message });
+    }
 })
-routes.put("/patient",async(req,res));
-routes.delete("/patient",async(req,res));
+routes.put("/patient", async(req, res));
+routes.delete("/patient", async(req, res));
