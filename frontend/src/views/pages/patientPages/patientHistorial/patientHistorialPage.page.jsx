344d74bccdf8dc8e46b8e75d3180554c15164632
Nicolas Xavier Herrera Medina <nicoxavier2013@hotmail.com>
2023-10-17 11:36:27 -0500

    Patients View

--- frontend/src/views/pages/patientPages/patientHistorial/patientHistorialPage.page.jsx
+++ frontend/src/views/pages/patientPages/patientHistorial/patientHistorialPage.page.jsx
@@ -1,17 +1,66 @@
 import "./patientHistorialPage.styles.css";
+import React, { Component } from 'react';
+import axios from 'axios';
+import { LocalStorageServices } from "../../../../services";
+import { ConstantLocalStorage } from "../../../../utils/constant";
 
-export default function PatientHistorialPage() {
-  return (
-    <div>
-     
-        <div class="content-info">
+
+export default class PatientHistorialPage extends Component {
+  state = {
+    citasPasadas: null, // Variable para almacenar los datos de la solicitud
+  };
+
+
+  componentDidMount() {
+    const token = LocalStorageServices.GetData(ConstantLocalStorage.token_key);
+    axios.get('/api/patient/pastGetAppointments', token)
+      .then(response => {
+        this.setState({ citasPasadas: response.data });
+        console.log(response.data)
+      })
+      .catch(error => {
+        console.error('Error al obtener datos:', error);
+      });
+  }
+
+
+  render() {
+    const { citasPasadas } = this.state;
+
+
+    return (
+      <div>
+        <div className="content-info">
           <h1>Historial</h1>
         </div>
 
-      <div>
-        <p>Esta es su página de historial clinico</p>
+
+        <div>
+          {citasPasadas ? (
+            <div>
+              <p>Historial clínico:</p>
+
+
+              {/* Renderiza la lista de citas pasadas */}
+              <ul>
+                {citasPasadas.map((cita, index) => (
+                  <li key={index}>{cita.date}</li>
+                ))}
+              </ul>
+
+
+
+
+             
+            </div>
+          ) : (
+            <p>Cargando datos...</p>
+          )}
+        </div>
       </div>
-    </div>
-    
-  );
+    );
+  }
 }
+
+
+
