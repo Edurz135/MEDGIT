0852c0d53b15bba6382fdf1c6248d5ac3ae8ae32
RBrunoChV <edu.rz@hotmail.com>
2023-11-03 19:55:32 -0500

    stylized page

--- frontend/src/views/pages/doctorPages/doctorPerfil/doctorPerfilPage.page.jsx
+++ frontend/src/views/pages/doctorPages/doctorPerfil/doctorPerfilPage.page.jsx
@@ -1,20 +1,22 @@
+import { useNavigate } from "react-router-dom";
 import "./doctorPerfilPage.styles.css";
-
+import { Button } from "antd";
 
 export default function DoctorPerfilPage() {
+  const navigate = useNavigate();
+  const handleDoctorAvailability = () => {
+    navigate("/auth/doctor/availability");
+  };
   return (
     <div>
-     
-        <div class="content-info">
-          <h1>Perfil</h1>
-        </div>
-     
-  
-    
+      <div class="content-info">
+        <h1>Perfil</h1>
+      </div>
+
       <div>
         <p>Esta es su página de perfil</p>
       </div>
+      <Button type="primary" onClick={handleDoctorAvailability}>Modificar disponibilidad</Button>
     </div>
-    
   );
 }
