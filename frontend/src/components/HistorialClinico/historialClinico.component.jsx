0272fa48c2ab8f4c0c6f40615bd901bde26fd60f
ValeriaEV <valeriaespiritu2003@gmail.com>
2023-12-01 08:31:56 -0500

    citasPendientesHistorialC

--- frontend/src/components/HistorialClinico/historialClinico.component.jsx
+++ frontend/src/components/HistorialClinico/historialClinico.component.jsx
@@ -1,18 +1,44 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Card } from "antd";
-import { useParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
+import { LocalStorageServices } from "../../services";
 
-export default function HistorialClinico(props) {
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
+export default function HistorialClinico() {
+  const [citaInfo, setCitaInfo] = useState(null);
+  let query = useQuery();
+  const citaId = query.get("citaId");
 
   useEffect(() => {
-    console.log(props.data)
-    // hacer una consulta de citas pasadas
-  }, [])
+    const fetchCitaInfo = async () => {
+      try {
+        const accessToken = await LocalStorageServices.GetData("accessToken");
+        const response = await axios.get(`http://localhost:3100/api/citas/${citaId}`, {
+          headers: { Authorization: accessToken },
+        });
+        setCitaInfo(response.data);
+      } catch (error) {
+        console.error("Error al obtener datos de la cita:", error);
+      }
+    };
+
+    if (citaId) {
+      fetchCitaInfo();
+    }
+  }, [citaId]);
+
+  if (!citaInfo) {
+    return <div>Cargando información de la cita...</div>;
+  }
+
   return (
     <div>
-      <p></p>
       <Card
-        title="Tipo de Cita"
+        title={`Tipo de Cita: ${citaInfo.tipoCita}`}
         bordered={true}
         headStyle={{
           background: "#3E5BA5",
@@ -22,8 +48,8 @@ export default function HistorialClinico(props) {
           boxShadow: "0px 6px 8px 0px rgba(0, 0, 0, 0.1)",
         }}
       >
-        <p>Cita en (especialidad)</p>
-        <p>#informacion del examen medico</p>
+        <p>Cita en: {citaInfo.especialidad}</p>
+        <p>Información del examen médico: {citaInfo.informacionExamenMedico}</p>
       </Card>
     </div>
   );
